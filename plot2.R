b2fa58474bec21e264da42f65e41d727184486e0
dliulhd <dliulhd@gmail.com>
2015-07-10 21:34:53 -0400

    comments added

--- plot2.R
+++ plot2.R
@@ -1,8 +1,8 @@
 # Run "InputData.R" file before this file
 
-# Please note that the author's computer uses French
-# The days of the week in the x-axis are "Jeu", "Ven" and "Sam" for "Thu", "Fri" and "Sat"
- 
+# Please note that since the system language is French, therefore
+# the days of the week shown in the x-axis are "Jeu", "Ven" and "Sam" for "Thu", "Fri" and "Sat"
+
 # Do Plot 2
 
 png("plot2.png",bg="transparent")
